8ec60fa965936c59b011e82ded2e45f9f350e4da
SRIVATHSAN GANESAN <srivathsan18901@gmail.com>
2023-07-10 15:44:00 +0530

    Add project files.

---
+++ AGV model/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace AGV_model
+{
+    partial class EP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EP));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.EnterBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(10, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Number of AGV\'s  -";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Variable Text", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(295, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(56, 38);
+            this.textBox1.TabIndex = 1;
+            // 
+            // EnterBtn
+            // 
+            this.EnterBtn.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EnterBtn.Location = new System.Drawing.Point(357, 15);
+            this.EnterBtn.Name = "EnterBtn";
+            this.EnterBtn.Size = new System.Drawing.Size(84, 36);
+            this.EnterBtn.TabIndex = 2;
+            this.EnterBtn.Text = "Enter";
+            this.EnterBtn.UseVisualStyleBackColor = true;
+            this.EnterBtn.Click += new System.EventHandler(this.EnterBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(4, 56);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1365, 692);
+            this.panel1.TabIndex = 3;
+            // 
+            // EP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1389, 760);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.EnterBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "EP";
+            this.Text = "EP";
+            this.Load += new System.EventHandler(this.EP_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button EnterBtn;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ AGV model/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AGV_model
+{
+    public partial class EP : Form
+    {
+        public EP()
+        {
+            InitializeComponent();
+            panel1.AutoScroll = true; // Set AutoScroll property to true for the panel
+
+            // Subscribe to the form's Load event
+            this.Load += EP_Load;
+            
+        }
+        
+        private void EnterBtn_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+
+            // Parse the user input
+            if (int.TryParse(textBox1.Text, out int numberOfBoxes))
+            {
+                // Validate the input range
+                if (numberOfBoxes >= 1 && numberOfBoxes <= 100)
+                {
+                    // Calculate the number of rows and columns
+                    int rows = (numberOfBoxes + 2) / 3;
+                    int columns = Math.Min(numberOfBoxes, 5);
+
+                    // Calculate the size of each GroupBox
+                    int groupBoxWidth = panel1.Width / columns - 8; // Adjust for margins
+                    int groupBoxHeight = 140;
+
+                    // Create the main TableLayoutPanel
+                    TableLayoutPanel mainTableLayoutPanel = new TableLayoutPanel();
+                    mainTableLayoutPanel.Dock = DockStyle.Fill;
+                    mainTableLayoutPanel.RowCount = rows;
+                    mainTableLayoutPanel.ColumnCount = columns;
+                    panel1.Controls.Add(mainTableLayoutPanel);
+
+                    // Create and add GroupBoxes to the main TableLayoutPanel
+                    for (int row = 0; row < rows; row++)
+                    {
+                        for (int col = 0; col < columns; col++)
+                        {
+                            int index = row * 5 + col + 1;
+
+                            if (index > numberOfBoxes)
+                                break;
+
+                            GroupBox groupBox = new GroupBox();
+                            groupBox.Text = "AGV- " + index.ToString();
+                            //groupBox.Size = new Size(260, 140);
+                            groupBox.Size = new Size(groupBoxWidth, groupBoxHeight);
+                            groupBox.Font = new Font(FontFamily.GenericSansSerif, 10, FontStyle.Italic);
+                            groupBox.ForeColor = Color.White;
+                            groupBox.BackColor = Color.FromArgb(150, 0, 0, 0); // Darker color, adjust the values as desired
+                            this.BackColor = Color.FromArgb(255, 0, 0, 0); // Set the same color as the form's transparency key
+
+                            // Calculate the position of the GroupBox
+                            int groupBoxX = col * (groupBoxWidth + 10); // Adjust for margins
+                            int groupBoxY = row * (groupBoxHeight + 10); // Adjust for margins
+                            groupBox.Location = new Point(groupBoxX, groupBoxY);
+
+                            // Create labels and TextBoxes inside the GroupBox
+                            Label startingPosXLabel = new Label();
+                            startingPosXLabel.Text = "Starting position X:";
+                            startingPosXLabel.Location = new Point(10, 20);
+                            startingPosXLabel.Size = new Size(180, 20);
+                            startingPosXLabel.BackColor = Color.Transparent;
+                            startingPosXLabel.ForeColor = Color.White;
+                            startingPosXLabel.Font = new Font(FontFamily.GenericSansSerif, 12, FontStyle.Italic);
+                            groupBox.Controls.Add(startingPosXLabel);
+
+                            TextBox startingPosTextBoxX = new TextBox();
+                            startingPosTextBoxX.Location = new Point(200, 20);
+                            startingPosTextBoxX.Size = new Size(50, 20);
+                            groupBox.Controls.Add(startingPosTextBoxX);
+
+                            Label startingPosYLabel = new Label();
+                            startingPosYLabel.Text = "Starting position Y:";
+                            startingPosYLabel.Location = new Point(10, 50);
+                            startingPosYLabel.Size = new Size(180, 20);
+                            startingPosYLabel.BackColor = Color.Transparent;
+                            startingPosYLabel.ForeColor = Color.White;
+                            startingPosYLabel.Font = new Font(FontFamily.GenericSansSerif, 12, FontStyle.Italic);
+                            groupBox.Controls.Add(startingPosYLabel);
+
+                            TextBox startingPosTextBoxY = new TextBox();
+                            startingPosTextBoxY.Location = new Point(200, 50);
+                            startingPosTextBoxY.Size = new Size(50, 20);
+                            groupBox.Controls.Add(startingPosTextBoxY);
+
+                            Label reachingPosXLabel = new Label();
+                            reachingPosXLabel.Text = "Reaching position X:";
+                            reachingPosXLabel.Location = new Point(10, 80);
+                            reachingPosXLabel.BackColor = Color.Transparent;
+                            reachingPosXLabel.ForeColor = Color.White;
+                            reachingPosXLabel.Font = new Font(FontFamily.GenericSansSerif, 12, FontStyle.Italic);
+                            reachingPosXLabel.Size = new Size(180, 20);
+                            groupBox.Controls.Add(reachingPosXLabel);
+
+                            TextBox reachingPosTextBoxX = new TextBox();
+                            reachingPosTextBoxX.Location = new Point(200, 80);
+                            reachingPosTextBoxX.Size = new Size(50, 20);
+                            groupBox.Controls.Add(reachingPosTextBoxX);
+
+                            Label reachingPosYLabel = new Label();
+                            reachingPosYLabel.Text = "Reaching position Y:";
+                            reachingPosYLabel.Location = new Point(10, 110);
+                            reachingPosYLabel .BackColor = Color.Transparent;
+                            reachingPosYLabel.ForeColor = Color.White;
+                            reachingPosYLabel.Font = new Font(FontFamily.GenericSansSerif, 12, FontStyle.Italic);
+                            reachingPosYLabel.Size = new Size(180, 20);
+                            groupBox.Controls.Add(reachingPosYLabel);
+
+                            TextBox reachingPosTextBoxY = new TextBox();
+                            reachingPosTextBoxY.Location = new Point(200, 110);
+                            reachingPosTextBoxY.Size = new Size(50, 20);
+                            groupBox.Controls.Add(reachingPosTextBoxY);
+
+                            // Add the GroupBox to the main TableLayoutPanel
+                            mainTableLayoutPanel.Controls.Add(groupBox, col, row);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a number between 1 and 100.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number.");
+            }
+        }
+
+        private void EP_Load(object sender, EventArgs e)
+        {
+            // Get the size of the desktop.
+            Size desktopSize = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Size;
+
+            //   this.DesktopBounds = new System.Drawing.Rectangle(0, 0, desktopSize.Width, desktopSize.Height);
+
+            int panelWidth = (int)(Screen.PrimaryScreen.Bounds.Width * 1); // Adjust the width as desired
+            int panelHeight = (int)(Screen.PrimaryScreen.Bounds.Height * 1); // Adjust the height as desired
+
+            panel1.Size = new Size(panelWidth, panelHeight);
+        }
+    }
+}
